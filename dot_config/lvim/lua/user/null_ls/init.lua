5b87b930cdb1c909d9f26204113b3da1627aa013
Sirio Negri <ziriuz84@gmail.com>
2023-09-11 08:53:17 +0200

    Fixes lvim config

--- dot_config/lvim/lua/user/null_ls/init.lua
+++ dot_config/lvim/lua/user/null_ls/init.lua
@@ -53,6 +53,7 @@ M.config = function()
     nls.builtins.formatting.cmake_format,
     nls.builtins.formatting.scalafmt,
     nls.builtins.formatting.sqlformat,
+    nls.builtins.formatting.phpcsfixer,
     nls.builtins.formatting.terraform_fmt.with {
       filetypes = { "terraform", "tf", "terraform-vars", "hcl" },
     },
