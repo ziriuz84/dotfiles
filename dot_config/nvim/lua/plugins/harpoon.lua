ab70b451fec736ecb25fc7b5b65d55fe29bacb4b
Sirio Negri <ziriuz84@gmail.com>
2023-11-07 23:15:24 +0100

    adds keymap for harpoon

--- dot_config/nvim/lua/plugins/harpoon.lua
+++ dot_config/nvim/lua/plugins/harpoon.lua
@@ -1,6 +1,22 @@
 return {
   {
+    "folke/which-key.nvim",
+    opts = {
+      defaults = {
+        ["<leader>H"] = { name = "+Harpoon" },
+      },
+    },
+  },
+  {
     "ThePrimeagen/harpoon",
     event = "VeryLazy",
+    keys = {
+      {
+        "<leader>Ha",
+        "<cmd>lua require('harpoon.mark').add_file()<CR>",
+        desc = "Add to Harpoon",
+      },
+      { "<leader>HH", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", desc = "Open Harpoon" },
+    },
   },
 }
