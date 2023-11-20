00a97a8f96f70f18f7c4f82fb99786602f80043c
Sirio Negri <sirio.negri@ssd.it>
2023-11-20 23:24:08 +0100

    Fixes formatter for php

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -5,15 +5,15 @@ return {
       local opts = {
         formatters_by_ft = {
           python = { "autoflake", "isort", "black", "autopep8" },
-          php = { "php-cs-fixer" },
+          php = { "php_cs_fixer" },
           go = { "gofmt", "goimports", "gomodifytags", "djlint" },
           rust = { "rustfmt" },
           javascript = { "prettier", "rustywind" },
           typescript = { "prettier", "rustywind" },
-          JSON = { "fixjson", "prettier" },
+          json = { "fixjson", "prettier" },
           sh = { "beautysh", "shfmt" },
-          blade = { "php-cs-fixer", "blade-formatter" },
-          YAML = { "prettier", "yamlfmt", "yamlfix" },
+          blade = { "php_cs_fixer", "blade_formatter" },
+          yaml = { "prettier", "yamlfmt", "yamlfix" },
         },
       }
       return opts
