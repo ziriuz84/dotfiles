b0cde99c3f3c7f494884f57ead67a58bd1609444
Sirio Negri <ziriuz84@gmail.com>
2024-01-25 21:30:38 +0100

    Better neotest integration

--- dot_config/nvim/lua/plugins/neotest.lua
+++ dot_config/nvim/lua/plugins/neotest.lua
@@ -4,14 +4,21 @@ return {
     lazy = true,
     dependencies = {
       "olimorris/neotest-phpunit",
+      "nvim-neotest/neotest-plenary",
+      "rouge8/neotest-rust",
+      "nvim-neotest/neotest-go",
+      "nvim-neotest/neotest-python",
     },
     config = function()
       require("neotest").setup({
         adapters = {
-          require("neotest-phpunit")
-        }
+          require("neotest-phpunit"),
+          require("neotest-plenary"),
+          require("neotest-rust"),
+          require("neotest-go"),
+          require("neotest-python"),
+        },
       })
-    end
-  }
-
+    end,
+  },
 }
