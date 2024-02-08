728e4703a48e0081ab84575bf5ed3ac5d0dc61e8
Sirio Negri <sirio.negri@ssd.it>
2024-02-08 17:44:35 +0100

    Better conform.nvim

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -5,15 +5,16 @@ return {
       local opts = {
         formatters_by_ft = {
           python = { "autoflake", "isort", "black", "autopep8" },
-          php = { "php_cs_fixer" },
+          php = { "php-cs-fixer" },
+          phtml = { "php-cs-fixer" },
           go = { "gofmt", "goimports", "gomodifytags", "djlint" },
           rust = { "rustfmt" },
           javascript = { "biome", "rustywind" },
           typescript = { "biome", "rustywind" },
-          html = { "prettierd", "rustywind" },
+          -- html = { "prettierd", "rustywind" },
           json = { "fixjson", "prettierd" },
           sh = { "beautysh", "shfmt" },
-          blade = { "php_cs_fixer", "blade_formatter" },
+          blade = { "blade-formatter" },
           yaml = { "prettierd", "yamlfmt", "yamlfix" },
         },
       }
