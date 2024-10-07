79d6e709c6f14971604b264ceee65fe968d61154
Sirio Negri <sirio.negri@ssd.it>
2024-10-07 07:00:26 +0200

    adds support to laravel

--- dot_config/nvim/lua/plugins/laravel.lua
+++ dot_config/nvim/lua/plugins/laravel.lua
@@ -1,20 +1,61 @@
 return {
   {
+    -- Add neotest-pest plugin for running PHP tests.
+    -- A package is also available for PHPUnit if needed.
+    "nvim-neotest/neotest",
+    dependencies = { "V13Axel/neotest-pest" },
+    opts = { adapters = { "neotest-pest" } },
+  },
+  {
+    -- Add a Treesitter parser for Laravel Blade to provide Blade syntax highlighting.
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "blade",
+        "php_only",
+      })
+    end,
+    config = function(_, opts)
+      vim.filetype.add({
+        pattern = {
+          [".*%.blade%.php"] = "blade",
+        },
+      })
+
+      require("nvim-treesitter.configs").setup(opts)
+      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      parser_config.blade = {
+        install_info = {
+          url = "https://github.com/EmranMR/tree-sitter-blade",
+          files = { "src/parser.c" },
+          branch = "main",
+        },
+        filetype = "blade",
+      }
+    end,
+  },
+  {
+    -- Add the Laravel.nvim plugin which gives the ability to run Artisan commands
+    -- from Neovim.
     "adalessa/laravel.nvim",
     dependencies = {
       "nvim-telescope/telescope.nvim",
       "tpope/vim-dotenv",
       "MunifTanjim/nui.nvim",
-      -- "nvimtools/none-ls.nvim",
+      "nvimtools/none-ls.nvim",
     },
     cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
     keys = {
-      { "<leader>cla", ":Laravel artisan<cr>" },
-      { "<leader>clr", ":Laravel routes<cr>" },
-      { "<leader>clm", ":Laravel related<cr>" },
+      { "<leader>za", ":Laravel artisan<cr>" },
+      { "<leader>zr", ":Laravel routes<cr>" },
+      { "<leader>zm", ":Laravel related<cr>" },
     },
     event = { "VeryLazy" },
     config = true,
+    opts = {
+      lsp_server = "intelephense",
+      features = { null_ls = { enable = false } },
+    },
   },
   {
     -- Add the blade-nav.nvim plugin which provides Goto File capabilities
