d37e538d55a4ff0832edfa4f2725fb9b05098587
Sirio Negri <ziriuz84@gmail.com>
2024-10-21 07:30:25 +0200

    Adds plugins from lazyvim

---
+++ dot_config/nvim-test/lua/custom/plugins/mini-indentscope.lua
@@ -0,0 +1,43 @@
+return {
+  -- Active indent guide and indent text objects. When you're browsing
+  -- code, this highlights the current level of indentation, and animates
+  -- the highlighting.
+  {
+    "echasnovski/mini.indentscope",
+    version = false, -- wait till new 0.7.0 release to put it back on semver
+    event = "LazyFile",
+    opts = {
+      -- symbol = "▏",
+      symbol = "│",
+      options = { try_as_border = true },
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = {
+          "alpha",
+          "dashboard",
+          "fzf",
+          "help",
+          "lazy",
+          "lazyterm",
+          "mason",
+          "neo-tree",
+          "notify",
+          "toggleterm",
+          "Trouble",
+          "trouble",
+        },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+    end,
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    event = "LazyFile",
+    opts = {
+      scope = { enabled = false },
+    },
+  },
+}
---
+++ dot_config/nvim-test/lua/custom/plugins/mini.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    'echasnovski/mini.nvim',
+    version = false,
+    config = function()
+      require('mini.nvim').setup()
+    end,
+  },
+}
