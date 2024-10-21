75e93ee340bf24c25de5d80cf79f45ac5be2911a
Sirio Negri <ziriuz84@gmail.com>
2024-10-21 07:59:01 +0200

    better plugins

---
+++ dot_config/nvim-test/lua/custom/plugins/rust.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^5', -- Recommended
+    lazy = false, -- This plugin is already lazy
+  },
+  {
+    'saecki/crates.nvim',
+    tag = 'stable',
+    config = function()
+      require('crates').setup()
+    end,
+  },
+  {
+    'rust-lang/rust.vim',
+    ft = 'rust',
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end,
+  },
+}
