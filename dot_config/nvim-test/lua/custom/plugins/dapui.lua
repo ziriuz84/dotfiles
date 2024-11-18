221357ee4588f22d10cd7d765d2508fbe1c006a3
Sirio Negri <ziriuz84@gmail.com>
2024-11-18 07:03:22 +0100

    better groups and dap-ui

--- dot_config/nvim-test/lua/custom/plugins/dapui.lua
+++ dot_config/nvim-test/lua/custom/plugins/dapui.lua
@@ -1,105 +1,13 @@
-local function setup()
-  local dap, dapui = require("dap"), require("dapui")
-
-  dapui.setup(
-    {
-      controls = {
-        element = "repl",
-        enabled = true,
-        icons = {
-          disconnect = "",
-          pause = "",
-          play = "",
-          run_last = "",
-          step_back = "",
-          step_into = "",
-          step_out = "",
-          step_over = "",
-          terminate = ""
-        }
-      },
-      element_mappings = {},
-      expand_lines = true,
-      floating = {
-        border = "single",
-        mappings = {
-          close = { "q", "<Esc>" }
-        }
-      },
-      force_buffers = true,
-      icons = {
-        collapsed = "",
-        current_frame = "",
-        expanded = ""
-      },
-      layouts = {
-        --{
-        --	elements = {
-        --		{
-        --			id = "scopes",
-        --			size = 0.25,
-        --		},
-        --		{
-        --			id = "breakpoints",
-        --			size = 0.25
-        --		},
-        --		{
-        --			id = "stacks",
-        --			size = 0.25
-        --		},
-        --		{
-        --			id = "watches",
-        --			size = 0.25
-        --		}
-        --	},
-        --	position = "left",
-        --	size = 40
-        --},
-        {
-          elements = {
-            --{
-            --	id = "repl",
-            --	size = 0.5
-            --},
-            --{
-            --	id = "console",
-            --	size = 0.5
-            --}
-          },
-          position = "bottom",
-          size = 10
-        }
-      },
-      mappings = {
-        edit = "e",
-        expand = { "<CR>", "<2-LeftMouse>" },
-        open = "o",
-        remove = "d",
-        repl = "r",
-        toggle = "t"
-      },
-      render = {
-        indent = 1,
-        max_value_lines = 100
-      }
-    }
-  )
-
-  dap.listeners.after.event_initialized["dapui_config"] = function()
-    dapui.open()
-  end
-  dap.listeners.before.event_terminated["dapui_config"] = function()
-    dapui.close()
-  end
-  dap.listeners.before.event_exited["dapui_config"] = function()
-    dapui.close()
-  end
-
-  vim.keymap.set("n", "<M-k>", require('dapui').eval, { noremap = true, silent = true })
-end
-
 return {
   "rcarriga/nvim-dap-ui",
-  dependencies = { "mfussenegger/nvim-dap" },
-  config = setup,
+  event = "VeryLazy",
+  dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"},
+  config = function()
+    require("dapui").setup()
+    vim.keymap.set("n", "<leader>Xu", require("dapui").toggle, { desc = "Toggle DAP UI" })
+    vim.keymap.set("n", "<leader>Xb", require("dap").toggle_breakpoint, { desc = "Toggle Breakpoint" })
+    vim.keymap.set("n", "<leader>Xc", require("dap").continue, { desc = "Continue execution" })
+    vim.keymap.set("n", "<leader>Xo", require("dap").step_over, { desc = "Step over" })
+    vim.keymap.set("n", "<leader>Xi", require("dap").step_into, { desc = "Step into" })
+  end
 }
