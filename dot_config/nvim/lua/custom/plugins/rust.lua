f6933a45cb393644f01ac2e4a6a0d54662f36ee8
Sirio Negri <ziriuz84@gmail.com>
2024-11-29 09:20:42 +0100

    formatting

--- dot_config/nvim/lua/custom/plugins/rust.lua
+++ dot_config/nvim/lua/custom/plugins/rust.lua
@@ -2,34 +2,34 @@ return {
   {
     'mrcjkb/rustaceanvim',
     version = '^5', -- Recommended
-    lazy = false,   -- This plugin is already lazy
+    lazy = false, -- This plugin is already lazy
   },
   {
     'saecki/crates.nvim',
     tag = 'stable',
     event = { 'BufRead Cargo.toml' },
     opts = {
-     completion = {
-          crates = {
-            enable = true,
-          },
-          cmp = {
-            enable = true,
-          }
-        },
-        lsp = {
+      completion = {
+        crates = {
           enable = true,
-          actions = true,
-          completion = true,
-          hover = true
         },
-        null_ls = {
+        cmp = {
           enable = true,
-          name = 'crates.nvim'
-        }
+        },
+      },
+      lsp = {
+        enable = true,
+        actions = true,
+        completion = true,
+        hover = true,
+      },
+      null_ls = {
+        enable = true,
+        name = 'crates.nvim',
+      },
     },
     config = function()
-      require('crates').setup() 
+      require('crates').setup()
     end,
   },
   {
