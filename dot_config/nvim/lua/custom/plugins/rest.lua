c403d120b622a3ce330a065a6bd3ef7a7a1bd6c1
Sirio Negri <ziriuz84@gmail.com>
2025-01-08 07:30:24 +0100

    adds rest.nvim

--- dot_config/nvim/lua/custom/plugins/rest.lua
+++ dot_config/nvim/lua/custom/plugins/rest.lua
@@ -1,3 +1,13 @@
 return {
   { 'mistweaverco/kulala.nvim', event = 'VeryLazy', opts = {} },
+  {
+    'rest-nvim/rest.nvim',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      opts = function(_, opts)
+        opts.ensure_installed = opts.ensure_installed or {}
+        table.insert(opts.ensure_installed, 'http')
+      end,
+    },
+  },
 }
