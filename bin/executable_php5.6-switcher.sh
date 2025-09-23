9b6582ac2afedc46d5a2757b526f4fe52e5756ee
Sirio Negri <ziriuz84@gmail.com>
2025-09-23 20:33:34 +0200

    removes old php switcher files

--- bin/executable_php5.6-switcher.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/env sh
-update-alternatives --set php /usr/bin/php5.6
-a2dismod php7.4
-a2enmod php5.6
-service apache2 restart
-php -i | grep "Loaded Configuration File"
--- bin/executable_php8.2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/env sh
-update-alternatives --set php /usr/bin/php8.2
-a2dismod php5.6
-a2dismod php7.4
-a2enmod php8.2
-systemctl restart apache2
-php -i | grep "Loaded Configuration File"
